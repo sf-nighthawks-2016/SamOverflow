5cc7d5ac32d4d1b5b661b56bdd73a206090f7086
Jason Sutter <jasonsutter87@gmail.com>
2016-06-23 18:16:22 -0700

    created a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
-# 20.times do
-#   post = Post.create!( title: Faker::Company.catch_phrase,
-#                username: Faker::Internet.user_name,
-#                comment_count: rand(1000),
-#                created_at: Time.now - rand(20000))
+80.times do
+	User.create(username: Faker::Name.name ,email: Faker::Internet.email, password: Faker::Internet.password)
+	Vote.create(user_id: rand(0..80), voteable_id: rand(0..80) , voteable_type: ["Post", "Comment"].sample  )
+	Post.create(user_id: rand(0..80), title: Faker::Lorem.word, content: Faker::Lorem.sentence)
+	Comment.create(user_id: rand(0..80), content: Faker::Lorem.sentence,  commentable_id: rand(0..80) , commentable_type: ["Post", "Votes"].sample)
+	Answer.create(user_id: rand(0..80), post_id: rand(0..80), content: Faker::Lorem.sentence)
+end
 
-#   vote_count = rand(100)
-#   vote_count.times do
-#     post.votes.create!(value: 1)
-#   end
-# end

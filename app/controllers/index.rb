ddc7baaadb671b741ecb80355c03593fa9a60477
Samuel Heinz <email": null>
2016-06-24 12:41:40 -0700

    Log in, log out, sign up works

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,43 @@ get '/' do
 	@posts = Post.all
 	erb :index
 end
+
+###signup page
+get '/users/new' do
+  erb :'/users/new'
+end
+
+####create new user
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save #&& @user.password != nil
+    redirect "/"
+  else
+    @error = "Invalid information, double check your email is correct or that you have not already created an account with this email"
+    erb :"/users/new"
+  end
+end
+
+### sign in page
+get '/authorization' do
+  erb :'/authorization/authorization'
+end
+
+
+### sign in user
+post "/authorization" do
+  @user = User.find_by(email: params[:user][:email])
+  if @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/"
+  else
+    @error = "Sorry, try a new password or email!"
+    erb :"/authorization/authorization"
+  end
+end
+
+post "/signout" do
+  session[:user_id] = nil
+  redirect "/"
+end
+

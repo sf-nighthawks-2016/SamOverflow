f35af97ca735669802f3921685b24e9ea11ab07f
Mikey Moore <email": null>
2016-06-24 11:54:30 -0700

    Add helper method for time, edit index page, ajax in progress for vote

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
-// $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-// });
+  $('button.fa.fa-sort-asc.vote-button.upvote-button').on('click', function(event){
+    event.preventDefault();
+    console.log("clicked")
+  })
+
+});
